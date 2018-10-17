46a96c770255a1f8b995318ac4c10aeabeb610d0
Cory Snider <cory.snider@fitbit.com>
2018-10-17 17:41:50 -0400

    Break up FDBTypes.ts into more manageable modules
    FDBTypes.ts has grown to nearly 1000 lines in length, making it unwieldy
    to read or edit. Break it up into more reasonably-sized chunks by moving
    the types for each aspect of the protocol into its own module.
    
    There should not be any impact on the public interface of the
    fdb-protocol package. All the same types are exported under the same
    names from the main module of the package. The only outwardly-visible
    change is the addition of some new exports for AppHost capabilities.

---
+++ packages/fdb-protocol/src/FDBTypes/ContentsList.ts
@@ -0,0 +1,54 @@
+import * as t from 'io-ts';
+
+import { StreamToken } from './BulkData';
+import { ComponentBundleKind, UUID } from './Structures';
+
+// Runtime types are variables which are used like types, which is
+// reflected in their PascalCase naming scheme.
+/* tslint:disable:variable-name */
+
+export const AppComponentContentsList = t.interface(
+  {
+    /**
+     * A map of all the files present in the app component at hand.
+     *
+     * The key is the path of the file, relative to the component bundle
+     * root.
+     */
+    files: t.dictionary(
+      t.string,
+      t.type(
+        {
+          /** The SHA-256 hash of the file's contents, encoded in base64. */
+          sha256: t.string,
+        },
+        'FileDescriptor',
+      ),
+      'files',
+    ),
+  },
+  'AppComponentContentsList',
+);
+export type AppComponentContentsList = t.TypeOf<typeof AppComponentContentsList>;
+
+export const AppComponentContentsRequest = t.interface(
+  {
+    /**
+     * Which app to query, based on UUID.
+     */
+    uuid: UUID,
+
+    /**
+     * Which component of the specified app to retrieve the list of
+     * contents from.
+     */
+    componentBundle: ComponentBundleKind,
+
+    /**
+     * The Stream that the Host will write the content list to.
+     */
+    stream: StreamToken,
+  },
+  'AppComponentContentsRequest',
+);
+export type AppComponentContentsRequest = t.TypeOf<typeof AppComponentContentsRequest>;
---
+++ packages/fdb-protocol/src/FDBTypes/Screenshot.ts
@@ -0,0 +1,63 @@
+import * as t from 'io-ts';
+
+import { StreamToken } from './BulkData';
+import { NonNegativeInteger } from './Structures';
+
+// Runtime types are variables which are used like types, which is
+// reflected in their PascalCase naming scheme.
+/* tslint:disable:variable-name */
+
+/**
+ * Capabilities specific to the capture of app screenshots.
+ */
+export const ScreenshotCapabilities = t.intersection(
+  [
+    t.partial({
+      /**
+       * The Host supports capturing screenshots and transferring them
+       * to the Debugger in-band with bulk data transfer by supporting
+       * the 'app.screenshot.stream.capture' request.
+       */
+      stream: t.boolean,
+    }),
+    t.interface({
+      /**
+       * The set of image formats that the Host supports for capturing
+       * screenshots.
+       */
+      imageFormats: t.array(t.string),
+    }),
+  ],
+  'ScreenshotCapabilities',
+);
+export type ScreenshotCapabilities = t.TypeOf<typeof ScreenshotCapabilities>;
+
+export const AppScreenshotStreamCaptureParams = t.interface(
+  {
+    /**
+     * The token for the stream on the Debugger that the Host should
+     * write the captured screenshot to.
+     */
+    stream: StreamToken,
+
+    /**
+     * The image format to encode the captured screenshot.
+     */
+    imageFormat: t.string,
+  },
+  'AppScreenshotStreamCaptureParams',
+);
+export type AppScreenshotStreamCaptureParams =
+   t.TypeOf<typeof AppScreenshotStreamCaptureParams>;
+
+export const AppScreenshotStreamCaptureResult = t.partial(
+  {
+    /**
+     * The total size of the image, in bytes, before transfer encoding.
+     */
+    length: NonNegativeInteger,
+  },
+  'AppScreenshotStreamCaptureResult',
+);
+export type AppScreenshotStreamCaptureResult =
+  t.TypeOf<typeof AppScreenshotStreamCaptureResult>;
