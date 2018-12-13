fd4671a9279db3cb6604539d41d9259e13dc395b
Cory Snider <cory.snider@fitbit.com>
2018-12-13 16:19:40 -0500

    Test with Node 8.0 and 10.0 in CI (#30)
    * Test with Node 8.0 and 10.0 in CI
    
    The sdk-cli unit tests are broken in Node 10.14.2, presumably because of
    a change that broke mock-fs. Lock the Node versions we run tests against
    to 8.0.0 and 10.0.0 to both test that everything runs against the lowest
    Node version we support and that these sorts of breakages don't happen
    again.
    
    Add an `engines` section to the sdk-cli package.json to be explicit
    about which Node versions we support. This matches the versions we
    support for fitbit-sdk-toolchain.
    
    * Bump locked package versions so it can install on node 8.0
    
    * Make Jest config compatible with Node 8.0
    
    * Enable yarn cache

--- packages/fdb-host/jest.config.js
+++ packages/fdb-host/jest.config.js
@@ -1,6 +1,5 @@
-module.exports = {
-  ...require('../jest.config.base'),
+module.exports = require('../jest.config.base')({
   // displayName must be set to work around buggy jest behaviour.
   // https://github.com/facebook/jest/issues/5597
   displayName: require('./package.json').name,
-};
+});
