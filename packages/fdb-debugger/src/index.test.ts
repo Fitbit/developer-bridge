c53475200c468968732dbe21a78e3e339c769e19
Liam McLoughlin <lmcloughlin@fitbit.com>
2018-12-19 21:08:17 +0000

    Fix incorrect verison in REPL quirk
    Signed-off-by: Liam McLoughlin <lmcloughlin@fitbit.com>

--- packages/fdb-debugger/src/index.test.ts
+++ packages/fdb-debugger/src/index.test.ts
@@ -713,8 +713,8 @@ const hostWithEvalSupport = (supported = true) => ({
 
 it.each([
   hostWithEvalSupport(),
-  { ...hostWithEvalSupport(), device: 'Higgs 27.31.2.30' },
-  { ...hostWithEvalSupport(), device: 'Meson 27.32.1.1' },
+  { ...hostWithEvalSupport(), device: 'Higgs 27.33.2.30' },
+  { ...hostWithEvalSupport(), device: 'Meson 27.34.1.1' },
 ])('detects when the host supports eval %#', async (response) => {
   const host = await init(response);
   expect(host.hasEvalSupport()).toBe(true);
@@ -723,8 +723,8 @@ it.each([
 it.each([
   {},
   hostWithEvalSupport(false),
-  { ...hostWithEvalSupport(), device: 'Higgs 27.31.1.30' },
-  { ...hostWithEvalSupport(), device: 'Meson 27.31.1.31' },
+  { ...hostWithEvalSupport(), device: 'Higgs 27.33.1.30' },
+  { ...hostWithEvalSupport(), device: 'Meson 27.33.1.31' },
 ])('detects when the host does not support eval %#', async (response) => {
   const host = await init(response);
   expect(host.hasEvalSupport()).toBe(false);
