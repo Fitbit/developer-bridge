75aaad529e25ebe97030d15da334282793622ccc
Rafael Sofi-zada <rafa.sofizadeh@gmail.com>
2021-10-26 14:45:43 +0100

    readJsonFile typing
    Signed-off-by: Rafael Sofi-zada <rafa.sofizadeh@gmail.com>

--- packages/sdk-cli/src/models/localRelay/util.ts
+++ packages/sdk-cli/src/models/localRelay/util.ts
@@ -1,8 +1,6 @@
 import { readFile } from 'fs/promises';
 
-export async function readJsonFile<
-  T extends Record<string, any> | number | string | boolean | null
->(path: string): Promise<T> {
+export async function readJsonFile<T = unknown>(path: string): Promise<T> {
   const contents = await readFile(path, { encoding: 'utf-8' });
   return JSON.parse(contents);
 }
