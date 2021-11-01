4f90686ed7c53cca878753d5cb2ea40ef6a2d3d6
Rafael Sofi-zada <rafa.sofizadeh@gmail.com>
2021-11-01 00:51:09 +0000

    Pipe child process' output/errors to a log file
    There are a couple of issues marked with [CONFUSING] and [TODO}, needs some experimentation with Node child_process and fs api to figure this out
    
    Signed-off-by: Rafael Sofi-zada <rafa.sofizadeh@gmail.com>

--- packages/sdk-cli/src/models/localRelay/const.ts
+++ packages/sdk-cli/src/models/localRelay/const.ts
@@ -2,6 +2,18 @@ import { tmpdir } from 'os';
 import { join } from 'path';
 
 export const RELAY_PKG_NAME = '@fitbit/local-developer-relay';
-export const RELAY_PID_FILE_NAME = 'fitbit.local-relay.json';
+export const RELAY_DIRECTORY_NAME = 'fitbit-local-relay';
 
-export const RELAY_PID_FILE_PATH = join(tmpdir(), RELAY_PID_FILE_NAME);
+export const RELAY_PID_FILE_NAME = 'pid.json';
+export const RELAY_PID_FILE_PATH = join(
+  tmpdir(),
+  RELAY_DIRECTORY_NAME,
+  RELAY_PID_FILE_NAME,
+);
+
+export const RELAY_LOG_FILE_NAME = 'logs.txt';
+export const RELAY_LOG_FILE_PATH = join(
+  tmpdir(),
+  RELAY_DIRECTORY_NAME,
+  RELAY_LOG_FILE_NAME,
+);
--- packages/sdk-cli/src/models/localRelay/index.ts
+++ packages/sdk-cli/src/models/localRelay/index.ts
@@ -1,3 +1,4 @@
+import { createWriteStream } from 'fs';
 import * as child_process from 'child_process';
 import {
   relayEntryPointPath,
@@ -6,6 +7,7 @@ import {
   readRelayInfo,
   ReadRelayInfoResult,
 } from './relayInfo';
+import { RELAY_LOG_FILE_PATH } from './const';
 
 export { RelayInfo } from './relayInfo';
 
@@ -21,17 +23,32 @@ export async function instance(): Promise<RelayInfo> {
 
 async function launch(): Promise<ReadRelayInfoResult> {
   const relayJsPath = await relayEntryPointPath();
+
+  // [CONFUSING]: Why is documentation using fs.open, when FileHandle is incompatible with fork() typing?
+  const out = createWriteStream(RELAY_LOG_FILE_PATH, { flags: 'a' });
+  const err = createWriteStream(RELAY_LOG_FILE_PATH, { flags: 'a' });
+
   // FORK:
   // https://nodejs.org/api/child_process.html#child_process_child_process_fork_modulepath_args_options
   // Unlike POSIX fork(), child_process.fork() creates a completely separate V8 process with its own memory.
   // Dangers of POSIX fork() (https://www.evanjones.ca/fork-is-dangerous.html) don't apply.
-  child_process.fork(relayJsPath, {
+  const relayChildProcess = child_process.fork(relayJsPath, {
     detached: true,
-    // We don't want to read parent's stdin from child process, but we want to share the same stdout/stderr.
-    // 'ipc' is fork()'s requirement.
-    // https://nodejs.org/api/child_process.html#child_process_child_process_fork_modulepath_args_options
-    stdio: ['ignore', 'inherit', 'inherit', 'ipc'],
+    /**
+     * 0: stdin  – We don't want to read parent's stdin
+     * 1: stdout – Log any child process messages to a file. Could be 'inherit', to let child's messages appear in the
+     *             parent's console, however, it isn't compatible with `detached: true`.
+     *             See: https://nodejs.org/api/child_process.html#optionsdetached
+     * 2: stderr – Log any child process errors to a file. See 1:stdout above.
+     * 3: ipc    – Having at least 1 'ipc' channel is a requirement for using `fork()`.
+     *             See: https://nodejs.org/api/child_process.html#child_processforkmodulepath-args-options -> options.stdio
+     *             [CONFUSING]: conflicts with https://nodejs.org/api/child_process.html#optionsdetached
+     */
+    stdio: ['ignore', out, err, 'ipc'],
   });
+
+  // [CONFUSING]: not sure if I need to do this
+  relayChildProcess.unref();
 
   return pollRelayInfo();
 }
