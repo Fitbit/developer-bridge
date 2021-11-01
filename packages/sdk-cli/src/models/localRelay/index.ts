5741be69c09be90932b08280954a050260eb8f4e
Rafael Sofi-zada <rafa.sofizadeh@gmail.com>
2021-11-01 01:59:41 +0000

    Better comments for forking Relay child process
    Signed-off-by: Rafael Sofi-zada <rafa.sofizadeh@gmail.com>

--- packages/sdk-cli/src/models/localRelay/index.ts
+++ packages/sdk-cli/src/models/localRelay/index.ts
@@ -12,6 +12,7 @@ import { RELAY_LOG_FILE_PATH } from './const';
 export { RelayInfo } from './relayInfo';
 
 export async function instance(): Promise<RelayInfo> {
+  // Connect to any existing Relay instance; if doesn't exist, launch a new one.
   const relayInfo = (await readRelayInfo()) || (await launch());
 
   if (!relayInfo) {
@@ -24,7 +25,8 @@ export async function instance(): Promise<RelayInfo> {
 async function launch(): Promise<ReadRelayInfoResult> {
   const relayJsPath = await relayEntryPointPath();
 
-  // [CONFUSING]: Why is documentation using fs.open, when FileHandle is incompatible with fork() typing?
+  // Official docs use fs.openSync, which returns a file descriptor integer.
+  // In our case, using a Writable Stream is more intuitive.
   const out = createWriteStream(RELAY_LOG_FILE_PATH, { flags: 'a' });
   const err = createWriteStream(RELAY_LOG_FILE_PATH, { flags: 'a' });
 
@@ -35,19 +37,23 @@ async function launch(): Promise<ReadRelayInfoResult> {
   const relayChildProcess = child_process.fork(relayJsPath, {
     detached: true,
     /**
-     * 0: stdin  – We don't want to read parent's stdin
-     * 1: stdout – Log any child process messages to a file. Could be 'inherit', to let child's messages appear in the
-     *             parent's console, however, it isn't compatible with `detached: true`.
-     *             See: https://nodejs.org/api/child_process.html#optionsdetached
+     * 0: stdin  – We don't want to read parent's stdin.
+     * 1: stdout – Log any child process messages to a file. Could also be 'inherit', to let child's messages appear in the
+     *             parent's console, however, it isn't compatible with `detached: true`
+     *             ("...provided with a stdio configuration that is not connected to the parent"):
+     *             https://nodejs.org/api/child_process.html#optionsdetached
+     *             Plus, we want to be able to read child process output even after the parent exits.
+     *             [TODO]: Ask StackOverflow about what exactly is meant by "stdio configuration", because even "inherit"
+     *             seems to work on test tasks. Maybe only "stdin" is meant?
      * 2: stderr – Log any child process errors to a file. See 1:stdout above.
-     * 3: ipc    – Having at least 1 'ipc' channel is a requirement for using `fork()`.
+     * 3: ipc    – Having an 'ipc' channel is a requirement for using `fork()`.
      *             See: https://nodejs.org/api/child_process.html#child_processforkmodulepath-args-options -> options.stdio
      *             [CONFUSING]: conflicts with https://nodejs.org/api/child_process.html#optionsdetached
      */
     stdio: ['ignore', out, err, 'ipc'],
   });
 
-  // [CONFUSING]: not sure if I need to do this
+  // unref()'ing child process allows the parent process (CLI) to exit without waiting for the child (Local Relay) to exit.
   relayChildProcess.unref();
 
   return pollRelayInfo();
