0fccaf212c27e28cc8060141f7195473ed7de2b0
Rafael Sofi-zada <rafa.sofizadeh@gmail.com>
2021-11-04 09:33:26 +0000

    Remove unnecessary tests
    Signed-off-by: Rafael Sofi-zada <rafa.sofizadeh@gmail.com>

--- packages/sdk-cli/src/models/localRelay/util.test.ts
+++ packages/sdk-cli/src/models/localRelay/util.test.ts
@@ -28,62 +28,34 @@ describe('isInt', () => {
   });
 });
 
-describe('readJsonFile', () => {
-  describe('actual file system', () => {
-    it('reads file and returns JSON', async () => {
-      const json = { data: 'random' };
-      const contents = JSON.stringify(json, null, 2);
+describe('readJsonFile – actual file system', () => {
+  it('reads file and returns JSON', async () => {
+    const json = { data: 'random' };
+    const contents = JSON.stringify(json, null, 2);
 
-      const path = join(tmpdir(), `readJsonFile-test-${RELAY_PID_FILE_NAME}`);
-      await fsPromises.writeFile(path, contents);
+    const path = join(tmpdir(), `readJsonFile-test-${RELAY_PID_FILE_NAME}`);
+    await fsPromises.writeFile(path, contents);
 
-      await expect(readJsonFile(path)).resolves.toEqual(json);
+    await expect(readJsonFile(path)).resolves.toEqual(json);
 
-      await fsPromises.unlink(path);
-    });
-
-    it("throws on file read error – file doesn't exist", async () => {
-      const path = join(tmpdir(), `readJsonFile-test-${RELAY_PID_FILE_NAME}`);
-      await expect(fsPromises.open(path, 'r')).rejects.toMatchObject({
-        code: 'ENOENT',
-      });
-      await expect(readJsonFile(path)).rejects.toThrowError();
-    });
-
-    it('throws on file JSON parse error', async () => {
-      const contents = 'not json at all';
-      const path = join(tmpdir(), `readJsonFile-test-${RELAY_PID_FILE_NAME}`);
-      await fsPromises.writeFile(path, contents);
-
-      await expect(readJsonFile(path)).rejects.toThrowError();
-
-      await fsPromises.unlink(path);
-    });
+    await fsPromises.unlink(path);
   });
 
-  describe('mock file system', () => {
-    it('reads file and returns JSON', async () => {
-      const json = { data: 'random' };
-      const contents = JSON.stringify(json, null, 2);
-
-      jest.spyOn(fsPromises, 'readFile').mockResolvedValueOnce(contents);
-
-      await expect(readJsonFile("path doesn't matter")).resolves.toEqual(json);
+  it("throws on file read error – file doesn't exist", async () => {
+    const path = join(tmpdir(), `readJsonFile-test-${RELAY_PID_FILE_NAME}`);
+    await expect(fsPromises.open(path, 'r')).rejects.toMatchObject({
+      code: 'ENOENT',
     });
+    await expect(readJsonFile(path)).rejects.toThrowError();
+  });
 
-    it("throws on file read error – file doesn't exist", async () => {
-      jest
-        .spyOn(fsPromises, 'readFile')
-        .mockRejectedValueOnce(new Error('generic read error'));
+  it('throws on file JSON parse error', async () => {
+    const contents = 'not json at all';
+    const path = join(tmpdir(), `readJsonFile-test-${RELAY_PID_FILE_NAME}`);
+    await fsPromises.writeFile(path, contents);
 
-      await expect(readJsonFile("path doesn't matter")).rejects.toThrowError();
-    });
+    await expect(readJsonFile(path)).rejects.toThrowError();
 
-    it('throws on file JSON parse error', async () => {
-      const contents = 'not json at all';
-      jest.spyOn(fsPromises, 'readFile').mockResolvedValueOnce(contents);
-
-      await expect(readJsonFile("path doesn't matter")).rejects.toThrowError();
-    });
+    await fsPromises.unlink(path);
   });
 });
