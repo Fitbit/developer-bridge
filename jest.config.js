fe2a93fb1a2a501e594b06a054edb3c4fa065a3b
Rafael Sofi-zada <rafa.sofizadeh@gmail.com>
2021-11-09 18:45:17 +0000

    Remove jest config changes introduced in f57bdf1
    Signed-off-by: Rafael Sofi-zada <rafa.sofizadeh@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   projects: ['<rootDir>/packages/*/jest.config.js'],
   coverageDirectory: '<rootDir>/coverage',
-  transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  transformIgnorePatterns: ['node_modules/(?!p-wait-for)'],
 };
