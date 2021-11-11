da81988fe619e08a87e3405e7315e8f9db562960
Rafael Sofi-zada <rafa.sofizadeh@gmail.com>
2021-11-11 07:52:40 +0000

    launch test refactoring – less focus on node API, more focus on logic, use real files
    Signed-off-by: Rafael Sofi-zada <rafa.sofizadeh@gmail.com>

--- packages/sdk-cli/src/models/localRelay/launch.test.ts
+++ packages/sdk-cli/src/models/localRelay/launch.test.ts
@@ -1,54 +1,101 @@
-import { Writable } from 'stream';
+import { createWriteStream, promises as fsPromises } from 'fs';
+import { ChildProcess } from 'child_process';
+import { join } from 'path';
+
 import { launch } from './launch';
 
 describe('launch', () => {
-  it('logs output to a log stream', async (done) => {
+  let subprocess: ChildProcess;
+  let logFilePath: string;
+
+  afterEach(async () => {
+    // 'SIGKILL' is guaranteed to terminate Node.js processes.
+    // https://nodejs.org/api/process.html#signal-events
+    if (!subprocess.kill('SIGKILL')) {
+      console.warn(
+        "Couldn't SIGKILL the subprocess. Most probably it has already exited.",
+      );
+    }
+
+    if (logFilePath) {
+      try {
+        await fsPromises.unlink(logFilePath);
+      } catch (error) {
+        if (error !== 'ENOENT') {
+          console.error(error);
+        }
+      }
+    }
+  });
+
+  it('spawns a process and logs output to a log file', (done) => {
     const logOutput = 'test output';
+    logFilePath = join(__dirname, './launch-test-output.txt');
+    const logFile = createWriteStream(logFilePath);
 
-    const nodeArgs = ['-e', `console.log('${logOutput}')`];
-    const subprocess = launch(nodeArgs, 'pipe');
+    // https://stackoverflow.com/a/44846808/6539857
+    // Without 'open' event spawn() won't accept the WriteStream, because
+    // "[log stream] must have an underlying descriptor (file streams do not until the 'open' event has occurred)"
+    // Related: https://github.com/nodejs/node-v0.x-archive/issues/4030
+    logFile.on('open', (fd) => {
+      const nodeArgs = ['-e', `console.log('${logOutput}')`];
+      subprocess = launch(nodeArgs, fd);
 
-    if (!subprocess.stdout) {
-      throw new Error(
-        "child_process.spawn()'ed subprocess doesn't have stdout",
-      );
-    }
+      subprocess.on('error', (error) => {
+        return done(error);
+      });
 
-    const logStream = new Writable({
-      write: (chunk: Buffer) => {
-        const data = chunk.toString();
-        // The default highWaterMark (buffer/chunk size) for streams is somewhere in KBs,
-        // which should be more than enough to get out logOutput in full.
-        expect(data).toMatch(logOutput);
-        subprocess.kill();
-        done();
-      },
+      subprocess.on('close', async () => {
+        try {
+          await expect(
+            fsPromises.readFile(logFilePath, { encoding: 'utf8' }),
+          ).resolves.toMatch(logOutput);
+        } catch (error) {
+          return done(error);
+        }
+
+        return done();
+      });
     });
 
-    subprocess.stdout.pipe(logStream);
+    logFile.on('error', (error) => {
+      return done(error);
+    });
   });
 
-  it('logs errors to a log stream', async (done) => {
-    const logError = 'test error';
-    const logStream = new Writable({
-      write: (chunk: Buffer) => {
-        const data = chunk.toString();
-        expect(data).toMatch(logError);
-        done();
-      },
+  // TODO: Separate log and error files?
+  it('spawns a process and logs error to a log file', (done) => {
+    const logOutput = 'test output';
+    logFilePath = join(__dirname, './launch-test-error-output.txt');
+    const logFile = createWriteStream(logFilePath);
+
+    // https://stackoverflow.com/a/44846808/6539857
+    // Without 'open' event spawn() won't accept the WriteStream, because
+    // "[log stream] must have an underlying descriptor (file streams do not until the 'open' event has occurred)"
+    // Related: https://github.com/nodejs/node-v0.x-archive/issues/4030
+    logFile.on('open', (fd) => {
+      const nodeArgs = ['-e', `console.log('${logOutput}')`];
+      subprocess = launch(nodeArgs, fd);
+
+      subprocess.on('error', (error) => {
+        return done(error);
+      });
+
+      subprocess.on('close', async () => {
+        try {
+          await expect(
+            fsPromises.readFile(logFilePath, { encoding: 'utf8' }),
+          ).resolves.toMatch(logOutput);
+        } catch (error) {
+          return done(error);
+        }
+
+        return done();
+      });
     });
 
-    const nodeArgs = ['-e', `console.error('${logError}')`];
-    const subprocess = launch(nodeArgs, 'pipe');
-
-    if (!subprocess.stderr) {
-      throw new Error(
-        "child_process.spawn()'ed subprocess doesn't have stderr",
-      );
-    }
-
-    subprocess.stderr.pipe(logStream);
+    logFile.on('error', (error) => {
+      return done(error);
+    });
   });
-
-  // TODO(future): add tests for detached & unref (i.e. whether parent and child processes are tied)
 });
--- packages/sdk-cli/src/models/localRelay/launch.ts
+++ packages/sdk-cli/src/models/localRelay/launch.ts
@@ -1,17 +1,24 @@
 import { ChildProcess, spawn } from 'child_process';
 import { Writable } from 'stream';
 
+// TODO: Separate log and error files?
 export function launch(
   nodeArgs: string[],
-  logStream: Writable | 'pipe' | 'ignore' | 'inherit',
+  logStream: Writable | number | 'pipe' | 'ignore' | 'inherit',
 ): ChildProcess {
   // Fork() doesn't support unref()
   const relayChildProcess = spawn('node', nodeArgs, {
     detached: true,
-    // Could be ['ignore', 'inherit', 'inherit'], to let child's messages appear in the parent's console.
-    // However, 'inherit' isn't compatible with 'detached: true':
-    // https://nodejs.org/api/child_process.html#optionsdetached
-    // "...provided with a stdio configuration that is not connected to the parent"):
+    /**
+     * 1. IMPORTANT: because both stdout and stderr are piped to the same file, it's a good idea to
+     *    explicitly differentiate log and error messages in the child process,
+     *    i.e. with "LOG: ..." and "ERROR: ...".
+     *
+     * 2. Could be ['ignore', 'inherit', 'inherit'], to let child's messages appear in the parent's console.
+     *    However, 'inherit' isn't compatible with 'detached: true':
+     *    https://nodejs.org/api/child_process.html#optionsdetached
+     *    "...provided with a stdio configuration that is not connected to the parent"):
+     */
     stdio: ['ignore', logStream, logStream],
   });
 
