a191fabd290d7d15f1f066f45723bf108efa9793
rafasofizada <rafa.sofizadeh@gmail.com>
2021-11-17 17:15:54 +0000

    package.json const TODO
    Signed-off-by: Rafael Sofi-zada <rafa.sofizadeh@gmail.com>

--- packages/sdk-cli/src/models/localRelay/const.ts
+++ packages/sdk-cli/src/models/localRelay/const.ts
@@ -1,6 +1,8 @@
 import { tmpdir } from 'os';
 import { join } from 'path';
 
+// TODO: add constant/s for package.json
+
 export const RELAY_PKG_NAME = '@fitbit/local-developer-relay';
 export const RELAY_DIRECTORY_NAME = 'fitbit-local-relay';
 export const RELAY_DIRECTORY_PATH = join(tmpdir(), RELAY_DIRECTORY_NAME);
